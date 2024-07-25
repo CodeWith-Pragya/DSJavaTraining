f7f2ddffc01151b5ad76a19afc48384393aa0556
CodeWith-Pragya <singhpragya7388@gmail.com>
2024-07-25 12:08:26 +0530

    addff

--- DSJavaTraining/src/TimeComplexityWithDebugging.java
+++ DSJavaTraining/src/TimeComplexityWithDebugging.java
@@ -1,9 +1,12 @@
 public class TimeComplexityWithDebugging {
     public static void main(String[] args) {
-        int i, n = 8;
-        for (i = 1; i <= n; i=i*2) {
-            System.out.println("Hello World!!!");
+        int [] number = { 1, 3, 5, 7, 11, 13 , 9  };
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            sum = sum + number[i];
         }
+        int avg =  (sum/number.length);
+        System.out.println(avg);
     }
 }
 
